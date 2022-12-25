32194e705cfa4038d755f318d16549f396bbeab6
Johan Torosjan <100092794+JohanTorosjan@users.noreply.github.com>
2022-12-25 13:32:05 +0100

    Add files via upload

---
+++ getrurality.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+
+if (__name__=="__main__"):
+    DFdensite=pd.read_csv("data vrac/grille_densite_2019.csv",sep=",",index_col="Libell? des communes")
+    DFdensite.index = DFdensite.index.map(str.upper)
+    hashtable = DFdensite.to_dict(orient='dict')['Typo degr? de densit?']
+    df=pd.read_csv("data vrac/effectifs-dans-les-enseignements-de-specialites-en-1ere-generale-national.csv",sep=";")
+    df["Rural"]=0
+    for index, row in df.iterrows():
+        if row["COMMUNE"] in hashtable:
+            df.loc[index,'Rural']=int(hashtable[row["COMMUNE"]])
+    print(df["Rural"])
+    df.to_csv("datacleaned/datawithrural.csv")
