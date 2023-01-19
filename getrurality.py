cd2cffaddd07635dee8923a4df572077a055595e
etienne <etienne.tillier@etu.umontpellier.fr>
2023-01-19 15:52:55 +0100

    maj et accents removed

--- getrurality.py
+++ getrurality.py
@@ -7,25 +7,14 @@ class getrurality:
 
     def launch():
         indiceRuralite = {"Communes très peu denses": 4, "Communes peu denses": 3, "Communes de densité intermédiaire" : 2, "Communes densément peuplées" : 1}
-        DFdensite=pd.read_csv("data vrac/grille_densite_2021.csv",sep=",")
-        #DFdensite=pd.read_csv("data vrac/grille_densite_2019.csv",sep=",",index_col="Libell? des communes")
-        for index, row in DFdensite.iterrows():
-            ville = DFdensite.loc[index, "NomCommune"]
-            DFdensite.loc[index, "NomCommune"] = str(ville).upper()
-            DFdensite.loc[index, "NomCommune"] = unidecode.unidecode(DFdensite.loc[index, "NomCommune"])
-        #hashtable = DFdensite.to_dict(orient='dict')['Typo degr? de densit?']
-        DFdensite.set_index("NomCommune")
+        DFdensite=pd.read_csv("data vrac/data sorted/grille_densite_2021.csv",sep=",",index_col="NomCommune")
+        print(DFdensite)
         hashtable = DFdensite.to_dict(orient='dict')['Libellé typologie']
         df=pd.read_csv("data vrac/effectifs-dans-les-enseignements-de-specialites-en-1ere-generale-national.csv",sep=";")
         df["Rural"]=0
-        print(hashtable)
         for index, row in df.iterrows():
-            if row["COMMUNE"] in DFdensite:
-                df.loc[index,'Rural']=int(indiceRuralite[DFdensite.loc[row["COMMUNE"], 'Libellé typologie']])
-                #df.loc[index,'Rural']=int(indiceRuralite[hashtable[row["COMMUNE"]]])
-                #df.loc[index,'Rural']=int(hashtable[row["COMMUNE"]])
-            #else:
-                #print(row["COMMUNE"])
+            if row["COMMUNE"] in hashtable:
+                df.loc[index,'Rural']=int(indiceRuralite[hashtable[row["COMMUNE"]]])
         df.to_csv("data cleaned/datawithrural3.csv")
 
         DFips=pd.read_csv("data vrac/fr-en-ips_lycees.csv",sep=";",index_col="UAI")
